3aa06b07b8ed27fcefe162007bbfb51a50b3f446
Shaoyao·琚 <jjzhiyuan@gmail.com>
2019-06-25 18:20:37 +0800

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,27 @@
 import config from './config'
+import { AppComponent } from './utils/midi';
 
-App({
+class MyApp extends AppComponent<any>{
+
+  globalData: any = {
+    config,
+  }
+
   onLaunch(options: any) {
     // Do something initial when launch.
-  },
+  }
+
   onShow(options: any) {
     // Do something when show.
-  },
+  }
+
   onHide() {
     // Do something when hide.
-  },
+  }
+
   onError(msg: any) {
     console.error(msg)
-  },
-  globalData: {
-    config,
-  },
-})
+  }
+}
+
+App(new MyApp())
