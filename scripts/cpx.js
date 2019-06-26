5a5253b9ae3895303a5e7ac31a04218b02e9c0b7
Shaoyao·琚 <jjzhiyuan@gmail.com>
2019-06-26 16:46:52 +0800

    add build command
    * add build command
    
    * update README & package.json
    
    * update cpx

--- scripts/cpx.js
+++ scripts/cpx.js
@@ -7,17 +7,62 @@ const path = require('path')
 
 const base = process.cwd()
 
-const evt = cpx.watch('src/**/*.{wxml,wxs,png,jpg,json,svg,js}', 'dist')
-evt.on('copy', (e) => {
-  const dstPath = path.resolve(base, e.dstPath)
-  const targetLen = fs.statSync(dstPath).size
-  if (!targetLen) {
-    // process.nextTick 无效
-    setTimeout(() => {
-      fs.writeFileSync(dstPath, fs.readFileSync(path.resolve(base, e.srcPath)))
-    }, 10)
+const source = 'src/**/*.{wxml,wxs,png,jpg,json,svg,js}'
+const outDir = 'dist'
+
+const Commands = {
+  copy() {
+    cpx.copySync(source, outDir)
+  },
+
+  watch() {
+    const evt = cpx.watch(source, outDir)
+
+    evt.on('copy', (e) => {
+      const dstPath = path.resolve(base, e.dstPath)
+      const targetLen = fs.statSync(dstPath).size
+      if (!targetLen) {
+        // process.nextTick 无效
+        setTimeout(() => {
+          fs.writeFileSync(dstPath, fs.readFileSync(path.resolve(base, e.srcPath)))
+        }, 10)
+      }
+    })
+
+    evt.on('watch-ready', () => {
+      console.log('cpx watching ready')
+    })
   }
-})
-evt.on('watch-ready', () => {
-  console.log('cpx watching ready')
-})
+}
+
+const commands = new Set([
+  'copy',
+  'watch'
+])
+
+let cmd = process.argv[2]
+let args = []
+
+if (commands.has(cmd)) {
+  args = process.argv.slice(3)
+} else {
+  cmd = '--help'
+}
+
+if (new Set(['--help', '-h']).has(cmd)) {
+  console.log(`
+    Usage
+      $ cpx <command>
+
+    Available commands
+      ${Array.from(commands).join(', ')}
+  `)
+  process.exit(0)
+}
+
+if (!Commands[cmd]) {
+  console.error('Wrong command')
+  process.exit(1)
+}
+
+Commands[cmd](...args)
